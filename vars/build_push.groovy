ec364eaeb682858608cc1c2e2a0ad1aa5b2c8b0d
Sarthak Sahni <129878528+sahni1@users.noreply.github.com>
2026-01-15 15:47:22 +0530

    Update build_push.groovy

--- vars/build_push.groovy
+++ vars/build_push.groovy
@@ -1,20 +1,36 @@
 // vars/buildAndPushDockerImage.groovy
 
-def call(String appName, String dockerImageName) {
+def call(String appName, String ecrRepositoryUri, String awsRegion, String awsCredentialsId) {
     node {
         stage('Checkout') {
             checkout scm
         }
 
-        stage('Build Docker Image') {
+        stage('Authenticate with AWS ECR') {
             script {
-                com.mycompany.DockerUtils.buildImage(dockerImageName)
+                // Authenticate to AWS ECR
+                com.mycompany.DockerUtils.authenticateECR(awsRegion, awsCredentialsId)
             }
         }
 
-        stage('Push Docker Image') {
+        stage('Build Docker Image') {
             script {
-                com.mycompany.DockerUtils.pushImage(dockerImageName)
+                // Build Docker image
+                com.mycompany.DockerUtils.buildImage(appName)
+            }
+        }
+
+        stage('Tag Docker Image') {
+            script {
+                // Tag the Docker image with the ECR repository URI
+                com.mycompany.DockerUtils.tagImage(appName, ecrRepositoryUri)
+            }
+        }
+
+        stage('Push Docker Image to ECR') {
+            script {
+                // Push the Docker image to ECR
+                com.mycompany.DockerUtils.pushImage(ecrRepositoryUri, appName)
             }
         }
     }
