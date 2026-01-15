09f3ecab0fd0b910fa59593e739387ca946964ef
Sarthak Sahni <129878528+sahni1@users.noreply.github.com>
2026-01-15 15:42:34 +0530

    Update DockerUtils.groovy

--- src/com/example/DockerUtils.groovy
+++ src/com/example/DockerUtils.groovy
@@ -3,15 +3,28 @@
 package com.mycompany
 
 class DockerUtils {
+    
+    // Method to authenticate with AWS ECR
+    static void authenticateECR(String awsRegion, String awsCredentialsId) {
+        withCredentials([aws(credentialsId: awsCredentialsId)]) {
+            sh """
+                aws ecr get-login-password --region ${awsRegion} | docker login --username AWS --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.${awsRegion}.amazonaws.com
+            """
+        }
+    }
+    
     // Method to build the Docker image
     static void buildImage(String dockerImageName) {
         sh "docker build -t ${dockerImageName} ."
     }
 
-    // Method to push the Docker image
-    static void pushImage(String dockerImageName) {
-        withDockerRegistry([credentialsId: 'dockerhub-credentials', url: 'https://index.docker.io/v1/']) {
-            sh "docker push ${dockerImageName}"
-        }
+    // Method to tag the Docker image
+    static void tagImage(String dockerImageName, String ecrRepositoryUri) {
+        sh "docker tag ${dockerImageName} ${ecrRepositoryUri}:${dockerImageName}"
+    }
+
+    // Method to push the Docker image to ECR
+    static void pushImage(String ecrRepositoryUri, String dockerImageName) {
+        sh "docker push ${ecrRepositoryUri}:${dockerImageName}"
     }
 }
