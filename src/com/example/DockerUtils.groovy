5516a99251ca66be356dc38be7fe6528e124ea37
Sarthak Sahni <129878528+sahni1@users.noreply.github.com>
2026-01-15 17:21:14 +0530

    Update DockerUtils.groovy

--- src/com/example/DockerUtils.groovy
+++ src/com/example/DockerUtils.groovy
@@ -1,28 +1,28 @@
-package com.example
+// package com.example
 
-class DockerUtils {
+// class DockerUtils {
 
-    // Method to authenticate to AWS ECR
-    static void authenticateECR(String awsRegion, String awsCredentialsId) {
-        // Use Jenkins' AWS credentials plugin to retrieve the credentials
-        withCredentials([aws(credentialsId: awsCredentialsId, region: awsRegion)]) {
-            // Run AWS CLI to authenticate to the ECR
-            sh "aws ecr get-login-password --region ${awsRegion} | docker login --username AWS --password-stdin ${ecrRepositoryUri}"
-        }
-    }
+//     // Method to authenticate to AWS ECR
+//     static void authenticateECR(String awsRegion, String awsCredentialsId) {
+//         // Use Jenkins' AWS credentials plugin to retrieve the credentials
+//         withCredentials([aws(credentialsId: awsCredentialsId, region: awsRegion)]) {
+//             // Run AWS CLI to authenticate to the ECR
+//             sh "aws ecr get-login-password --region ${awsRegion} | docker login --username AWS --password-stdin ${ecrRepositoryUri}"
+//         }
+//     }
 
-    // Method to build the Docker image
-    static void buildImage(String dockerImageName) {
-        sh "docker build -t ${dockerImageName} ."
-    }
+//     // Method to build the Docker image
+//     static void buildImage(String dockerImageName) {
+//         sh "docker build -t ${dockerImageName} ."
+//     }
 
-    // Method to tag the Docker image
-    static void tagImage(String dockerImageName, String ecrRepositoryUri) {
-        sh "docker tag ${dockerImageName} ${ecrRepositoryUri}:${dockerImageName}"
-    }
+//     // Method to tag the Docker image
+//     static void tagImage(String dockerImageName, String ecrRepositoryUri) {
+//         sh "docker tag ${dockerImageName} ${ecrRepositoryUri}:${dockerImageName}"
+//     }
 
-    // Method to push the Docker image to ECR
-    static void pushImage(String ecrRepositoryUri, String dockerImageName) {
-        sh "docker push ${ecrRepositoryUri}:${dockerImageName}"
-    }
-}
+//     // Method to push the Docker image to ECR
+//     static void pushImage(String ecrRepositoryUri, String dockerImageName) {
+//         sh "docker push ${ecrRepositoryUri}:${dockerImageName}"
+//     }
+// }
