33f754960957877949bebb4ae56d33c43d2602b0
Sarthak Sahni <129878528+sahni1@users.noreply.github.com>
2026-01-15 16:40:22 +0530

    Update build_push.groovy

--- vars/build_push.groovy
+++ vars/build_push.groovy
@@ -6,33 +6,24 @@ def call(String appName, String ecrRepositoryUri, String awsRegion, String awsCr
 
         stage('Authenticate with AWS ECR') {
             script {
-                // Authenticate to AWS ECR
-                withCredentials([string(credentialsId: awsCredentialsId, variable: 'AWS_ACCESS_KEY_ID'),
-                                 string(credentialsId: awsCredentialsId, variable: 'AWS_SECRET_ACCESS_KEY')]) {
-                    sh """
-                        aws ecr get-login-password --region ${awsRegion} | docker login --username AWS --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.${awsRegion}.amazonaws.com
-                    """
-                }
+                com.example.DockerUtils.authenticateECR(awsRegion, awsCredentialsId)
             }
         }
 
         stage('Build Docker Image') {
             script {
-                // Use DockerUtils to build the Docker image
                 com.example.DockerUtils.buildImage(appName)
             }
         }
 
         stage('Tag Docker Image') {
             script {
-                // Use DockerUtils to tag the Docker image
                 com.example.DockerUtils.tagImage(appName, ecrRepositoryUri)
             }
         }
 
         stage('Push Docker Image to ECR') {
             script {
-                // Use DockerUtils to push the Docker image to ECR
                 com.example.DockerUtils.pushImage(ecrRepositoryUri, appName)
             }
         }
